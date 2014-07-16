59d00671fe79c7d12fa2189b838dec20796d9932
Teresa de la Torre <teresa.delatorre@kaleidos.net>
2014-07-16 13:07:44 +0200

    Update admin

--- src/sound_caravan_web/admin.py
+++ src/sound_caravan_web/admin.py
@@ -3,20 +3,11 @@ from django.contrib.contenttypes import generic
 from .models import *
 
 
-class ImageAdmin(admin.ModelAdmin):
-    model = Image
-    list_display = ('title',)
-
-
-class VideoAdmin(admin.ModelAdmin):
-    model = Video
-    list_display = ('title',)
-
-
-admin.site.register(Image, ImageAdmin)
-admin.site.register(Video, VideoAdmin)
+admin.site.register(Image)
+admin.site.register(Video)
 admin.site.register(Home)
 admin.site.register(AboutUs)
 admin.site.register(Member)
+admin.site.register(Tour)
 admin.site.register(Event)
 admin.site.register(Contact)
