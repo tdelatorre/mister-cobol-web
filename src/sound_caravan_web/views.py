e61596d0b0f26797e0ebd6848784f2c42f5cb516
Teresa de la Torre <teresa.delatorre@kaleidos.net>
2014-07-16 13:08:19 +0200

    Add to context in view

--- src/sound_caravan_web/views.py
+++ src/sound_caravan_web/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from .models import *
 
 
 class LandingView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        home = Home.objects.filter(main_video__for_home=True)
+        about_us = AboutUs.objects.all()
+        members = Member.objects.all()
+        tour = Tour.objects.all()
+        events = Event.objects.all()
+        images = Image.objects.filter(for_gallery=True)
+        video_list = Video.objects.filter(for_home=False)
+        contact = Contact.objects.all()
+
+        context = {
+            'home': home,
+            'about_us': about_us,
+            'members': members,
+            'tour': tour,
+            'events': events,
+            'images': images,
+            'video_list': video_list,
+            'contact': contact,
+        }
+
+        return context
+
+
+
 
 
