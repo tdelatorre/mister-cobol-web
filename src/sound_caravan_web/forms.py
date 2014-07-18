7e7067eed5822cdedd80cd54f1b07234e5865a8e
Teresa de la Torre <teresa.delatorre@kaleidos.net>
2014-07-18 11:45:02 +0200

    Add textarea to contactform

--- src/sound_caravan_web/forms.py
+++ src/sound_caravan_web/forms.py
@@ -6,4 +6,4 @@ class ContactForm(forms.Form):
     name = forms.CharField(label=_('Nombre'), max_length=100)
     email = forms.EmailField(label=_('E-mail'), max_length=150)
     subject = forms.CharField(label=_('Asunto'), max_length=200)
-    message = forms.CharField(label=_('Mensaje'))
+    message = forms.CharField(label=_('Mensaje'), widget=forms.Textarea)
